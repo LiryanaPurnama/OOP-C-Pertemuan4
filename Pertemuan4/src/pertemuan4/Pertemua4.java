b1a39b881dbba991685a130a461d0002df4265c0
LiryanaPurnama <liryanapurnama28@gmail.com>
2025-04-12 14:20:45 +0800

    Pertemuan4

---
+++ Pertemuan4/src/pertemuan4/Pertemua4.java
@@ -0,0 +1,23 @@
+package pertemuan4;
+
+/**
+ *
+ * @author Liryana
+ * TGL: 12 April 2025 
+*/
+public class Pertemua4 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String[] NMA = new String[5];
+        
+        NMA[0]="Liryana";
+        NMA[1]="Ari";
+        NMA[2]="Edy";
+        NMA[3]="Devi";
+        NMA[4]="Yunita";
+    }
+    
+}
